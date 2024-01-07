4d8fe361e59a09c7414ff0ed506abd2ccc87f590
Sina Reyhani <46113213+sinareyhani@users.noreply.github.com>
2024-01-07 17:08:09 +0330

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,296 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="maldives">MALDIVES</div>
+        </div>
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <div className="text-wrapper">PORTUGAL</div>
+        </div>
+        <div className="overlap-2">
+          <div className="rectangle" />
+          <div className="text-wrapper-2">PARIS</div>
+        </div>
+        <div className="overlap-3">
+          <div className="overlap-4">
+            <div className="rectangle-2" />
+            <div className="FIND-DINING">Find Dining</div>
+            <div className="group">
+              <div className="text-wrapper-3">Venus &amp; more</div>
+              <img className="line" alt="Line" src="line-5.svg" />
+              <img className="img" alt="Line" src="line-6.svg" />
+            </div>
+          </div>
+          <div className="overlap-5">
+            <div className="rectangle-3" />
+            <div className="ACCOMMODATION">Accommodation</div>
+            <div className="group-2">
+              <div className="hotels-hostels">Hotels &amp; Hostels</div>
+              <img className="line" alt="Line" src="line-13.svg" />
+              <img className="img" alt="Line" src="line-14.svg" />
+            </div>
+          </div>
+          <div className="overlap-6">
+            <div className="rectangle-2" />
+            <div className="SWEET-DELIGHT">Sweet Delight</div>
+            <div className="group-3">
+              <div className="text-wrapper-3">Cakes &amp; more</div>
+              <img className="line-2" alt="Line" src="line-9.svg" />
+              <img className="img" alt="Line" src="line-10.svg" />
+            </div>
+          </div>
+          <div className="overlap-7">
+            <div className="rectangle-2" />
+            <div className="PHOTOGRAPHY">Photography</div>
+            <div className="group-4">
+              <div className="text-wrapper-4">Art of shadows</div>
+              <img className="line-2" alt="Line" src="line-3.svg" />
+              <img className="img" alt="Line" src="line-4.svg" />
+            </div>
+          </div>
+          <div className="overlap-8">
+            <div className="rectangle-2" />
+            <div className="ESSENTIALS">Essentials</div>
+            <div className="group-5">
+              <div className="text-wrapper-5">Wedding Checklist</div>
+              <img className="line-3" alt="Line" src="line-7.svg" />
+              <img className="img" alt="Line" src="line-8.svg" />
+            </div>
+          </div>
+          <div className="overlap-9">
+            <div className="rectangle-4" />
+            <div className="DECORATION">Decoration</div>
+            <div className="group-6">
+              <div className="text-wrapper-3">Dreamy Decor</div>
+              <img className="line" alt="Line" src="line-11.svg" />
+              <img className="img" alt="Line" src="line-12.svg" />
+            </div>
+          </div>
+          <div className="overlap-10">
+            <div className="rectangle-2" />
+            <div className="WEDDING-RINGS">Wedding Rings</div>
+            <div className="group-4">
+              <div className="text-wrapper-3">Cakes &amp; more</div>
+              <img className="line-2" alt="Line" src="line-9-2.svg" />
+              <img className="img" alt="Line" src="line-10-2.svg" />
+            </div>
+          </div>
+          <div className="overlap-11">
+            <div className="rectangle-2" />
+            <div className="ACCESSORIES">Accessories</div>
+            <div className="group-4">
+              <div className="text-wrapper-3">Cakes &amp; more</div>
+              <img className="line-2" alt="Line" src="line-9-4.svg" />
+              <img className="img" alt="Line" src="line-10-4.svg" />
+            </div>
+          </div>
+          <div className="overlap-12">
+            <div className="rectangle-4" />
+            <div className="COUTURE">Couture</div>
+            <div className="group-7">
+              <div className="text-wrapper-3">Cakes &amp; more</div>
+              <img className="line-2" alt="Line" src="line-9-3.svg" />
+              <img className="img" alt="Line" src="line-10-3.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-13">
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <div className="rectangle-5" />
+              <div className="rectangle-6" />
+            </div>
+          </div>
+          <div className="overlap-wrapper">
+            <div className="overlap-group-3">
+              <div className="div-wrapper">
+                <div className="overlap-group-3">
+                  <div className="rectangle-7" />
+                  <div className="rectangle-8" />
+                </div>
+              </div>
+              <img className="about" alt="About" src="about-2-1.png" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-14">
+          <div className="rectangle-9" />
+          <div className="login">LOGIN</div>
+          <img className="group-8" alt="Group" src="group.png" />
+          <div className="text-wrapper-6">instaplanned.com</div>
+          <p className="free-shipping-on">free shipping on international orders of $150+</p>
+          <img className="instaplanned" alt="Instaplanned" src="instaplanned-2.png" />
+        </div>
+        <img className="line-4" alt="Line" src="line-1.svg" />
+        <div className="home">HOME</div>
+        <div className="pages">PAGES</div>
+        <div className="shop">SHOP</div>
+        <div className="listings">LISTINGS</div>
+        <div className="blog">BLOG</div>
+        <div className="contact">CONTACT</div>
+        <div className="text-wrapper-7">Search</div>
+        <img className="line-5" alt="Line" src="line-2.svg" />
+        <div className="text-wrapper-8">WEDDING VENDORS. INSTANTLY BOOKED!</div>
+        <div className="group-wrapper">
+          <img className="group-9" alt="Group" src="image.png" />
+        </div>
+        <div className="img-wrapper">
+          <img className="group-10" alt="Group" src="group-219.png" />
+        </div>
+        <div className="vector-wrapper">
+          <img className="vector" alt="Vector" src="vector.svg" />
+        </div>
+        <div className="overlap-15">
+          <img className="vector-2" alt="Vector" src="image.svg" />
+        </div>
+        <img className="vector-3" alt="Vector" src="vector-2.svg" />
+        <div className="overlap-16">
+          <img className="wedding" alt="Wedding" src="wedding-gb50aeb8d0-1920-1.png" />
+          <div className="rectangle-10" />
+          <div className="text-wrapper-9">SPECIAL OCCASION</div>
+          <div className="listings-2">listings</div>
+          <div className="rectangle-11" />
+          <div className="rectangle-12" />
+          <div className="rectangle-13" />
+          <div className="rectangle-14" />
+          <div className="rectangle-15" />
+          <div className="text-wrapper-10">Keyword</div>
+          <div className="text-wrapper-11">Categories</div>
+          <div className="text-wrapper-12">Location</div>
+          <img className="vector-4" alt="Vector" src="vector-3.svg" />
+          <img className="vector-5" alt="Vector" src="vector-4.svg" />
+          <img className="vector-6" alt="Vector" src="vector-5.svg" />
+          <div className="text-wrapper-13">Search</div>
+        </div>
+        <p className="planner-of-your">PLANNER OF YOUR PERFECT WEDDING</p>
+        <div className="heavenly-places">HEAVENLY PLACES</div>
+        <div className="reading-time">READING TIME</div>
+        <div className="wedding-destinations">WEDDING DESTINATIONS</div>
+        <div className="moments-you-will">MOMENTS YOU WILL CHERISH</div>
+        <p className="it-is-a-long">
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking
+          at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using
+          <br />
+          Content here,
+          <br />
+          <br />
+          <br />
+          content here&#39;, making it look like readable English. Many desktop publishing packages and web page editors
+          now use Lorem Ipsum as their default model text.
+        </p>
+        <div className="overlap-17">
+          <div className="rectangle-16" />
+          <div className="rectangle-17" />
+          <div className="text-wrapper-14">More About</div>
+        </div>
+        <div className="overlap-18">
+          <div className="ellipse" />
+          <div className="ellipse-2" />
+          <img className="polygon" alt="Polygon" src="polygon-1.svg" />
+        </div>
+        <img className="banner-img" alt="Banner img" src="h1-banner-img-04-800x332.png" />
+        <img className="h-banner-img" alt="Banner img" src="h1-banner-img-02-800x330.png" />
+        <img className="banner-img-2" alt="Banner img" src="h1-banner-img-03-800x332.png" />
+        <div className="overlap-19">
+          <div className="rectangle-18" />
+          <div className="march">MARCH 27, 2022</div>
+        </div>
+        <div className="overlap-20">
+          <div className="rectangle-18" />
+          <div className="december">DECEMBER 24, 2022</div>
+        </div>
+        <div className="overlap-21">
+          <div className="rectangle-18" />
+          <div className="march">MARCH 27, 2022</div>
+        </div>
+        <p className="by-loren-evans">
+          <span className="span">By Loren Evans - Wedding decoration</span>
+        </p>
+        <div className="text-wrapper-15">Read more</div>
+        <p className="span-wrapper">
+          <span className="span">By Loren Evans - Wedding decoration</span>
+        </p>
+        <div className="text-wrapper-16">Read more</div>
+        <p className="p">
+          <span className="span">By Loren Evans - Wedding decoration</span>
+        </p>
+        <div className="text-wrapper-17">Read more</div>
+        <div className="elegant-invitations">ELEGANT INVITATIONS</div>
+        <div className="elegant-invitations-2">ELEGANT INVITATIONS</div>
+        <div className="elegant-invitations-3">ELEGANT INVITATIONS</div>
+        <p className="text-wrapper-18">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad
+        </p>
+        <p className="text-wrapper-19">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad
+        </p>
+        <p className="text-wrapper-20">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad
+        </p>
+        <img className="line-6" alt="Line" src="line-15.svg" />
+        <img className="line-7" alt="Line" src="line-16.svg" />
+        <img className="line-8" alt="Line" src="line-17.svg" />
+        <div className="overlap-22">
+          <img className="element" alt="Element" src="2-1.png" />
+          <div className="group-11">
+            <div className="group-12">
+              <div className="text-wrapper-21">yourgmail@gmail.com</div>
+              <img className="vector-7" alt="Vector" src="vector-6.svg" />
+            </div>
+            <div className="group-13">
+              <p className="text-wrapper-22">The Royale, 188 East 64th Street</p>
+              <img className="vector-8" alt="Vector" src="vector-7.svg" />
+            </div>
+            <div className="group-14">
+              <img className="vector-9" alt="Vector" src="vector-8.svg" />
+              <div className="text-wrapper-23">03004003954</div>
+            </div>
+          </div>
+          <div className="quick-q-a">Quick Q&amp;A</div>
+          <div className="text-wrapper-24">Latest Trends</div>
+          <div className="quick-q-a-2">Quick Q&amp;A</div>
+          <p className="how-to-find-the">
+            <span className="text-wrapper-25">
+              How to find the right photographer? <br />
+              What are the best catering deals? <br />
+              Can I order my wedding dress online? <br />
+              How do I make the ideal guest list? <br />
+              How do I pick the perfect gown? <br />
+              How do I pick a wedding destination?
+            </span>
+          </p>
+          <p className="maldives-wedding">
+            Maldives Wedding Resorts
+            <br />
+            March 27, 2020
+            <br />
+            Anna and Dave’s Wedding
+            <br />
+            March 27, 2020
+            <br />
+            Bridesmaid Bouquets
+            <br />
+            March 27, 2020
+          </p>
+          <img className="element-2" alt="Element" src="93259029-230834311454245-3110453604024920153-nfull.png" />
+          <img className="element-3" alt="Element" src="93480842-158604122129832-5255219416961784773-nfull.png" />
+          <img className="element-4" alt="Element" src="93576011-150128609774507-4323418451066865625-nfull-2.png" />
+          <img className="element-5" alt="Element" src="92461400-216432966327819-8760476837616284731-nfull-2.png" />
+          <img className="element-6" alt="Element" src="92664496-1077602985958191-2959711326859322702-nfull.png" />
+          <img className="element-7" alt="Element" src="92769866-156556212376864-4587455272969417128-nfull.png" />
+        </div>
+        <img className="vector-10" alt="Vector" src="vector-9.svg" />
+      </div>
+    </div>
+  );
+};
